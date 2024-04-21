49e0ee60fc25feb0eba1d390f53ca9f897a96fee
Antim <antimsarker0@gmail.com>
2024-04-21 19:22:53 +0600

    row has added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import Main from './../components/Main';
 import Navbar from './../components/Navbar';
-
+import Row from './../components/Row';
+import requests from '../Request.js'
 const Home = () => {
     return (
         <div>
             <Navbar></Navbar>
            <Main></Main> 
+           <Row
+           title='Up Coming' fetchURL={requests.requestUpcoming}
+           ></Row>
+           <Row
+           title='Popular' fetchURL={requests.requestPopular}
+           ></Row>
+           <Row
+           title='Trending' fetchURL={requests.requestTrending}
+           ></Row>
+           <Row
+           title='Top Rated' fetchURL={requests.requestTopRated}
+           ></Row>
+           <Row
+           title='Horror' fetchURL={requests.requestHorror}
+           ></Row>
         </div>
     );
 };
